0bb6847e48f4b0ea5d43e38690b96fb537a90d4c
Peter Ballok <pballok@gmail.com>
2018-05-24 18:46:57 +0200

    Update lists.py

--- guides/lists.py
+++ guides/lists.py
@@ -2,13 +2,9 @@
 hozzavalok = ["Bagoly Hónaljszőr", "Csigalekvár", "Szalamandra szem", "Pókláb",
               "Béka nagylábujj", "Mocsári bűzgomba"]
 
-
-
 # Lista adott eleme:
 print(hozzavalok[2]) # Szalamandra szem
 print(hozzavalok[0]) # Bagoly Hónaljszőr
-
-
 
 # Két lista összeadása:
 teljes_lista = hozzavalok + ["Medveböfi", "Pipacsporzó"]
@@ -16,12 +12,9 @@ print(teljes_lista) # ["Bagoly Hónaljszőr", "Csigalekvár", "Szalamandra szem"
                     # "Pókláb", "Béka nagylábujj", "Mocsári bűzgomba",
                     # "Medveböfi", "Pipacsporzó"]
 
-
 # Lista hossza
 print(len(hozzavalok))   # 6
 print(len(teljes_lista)) # 8
-
-
 
 # Lista tartományok (ranges)
 print(hozzavalok[1:3]) # ["Csigalekvár", "Szalamandra szem"]
