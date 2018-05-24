ba51a87d37522776a6ac5b0f06c97a95842d37c2
Peter Ballok <pballok@pinkpanther-localdomain.3amlabs.net>
2018-05-24 10:08:09 +0200

    Add max maximum turn calculation to number_guess

--- games/number_guess/number_guess.py
+++ games/number_guess/number_guess.py
@@ -1,15 +1,18 @@
 import random
+import math
 
 MAX_SECRET = 100
 
 secret = random.randint(1, MAX_SECRET)
 current_turn = 1
 latest_guess = 0
+max_turns = int(math.ceil(math.log2(MAX_SECRET))) + 1
 
 print("I thought of a number between 1 and {}, try to guess it!".format(MAX_SECRET))
-while latest_guess != secret:
+
+while latest_guess != secret and current_turn <= max_turns:
     try:
-        latest_guess = int(input("Turn {}. Your guess: ".format(current_turn)))
+        latest_guess = int(input("Turn {}/{}. Your guess: ".format(current_turn, max_turns)))
     except Exception:
         print("Please enter numbers only.")
         continue
@@ -20,4 +23,7 @@ while latest_guess != secret:
         print("That is a too large number!")
     current_turn = current_turn + 1
 
-print("Congratualtions, it took you {} turns to guess the number {}".format(current_turn - 1, secret))
+if latest_guess == secret:
+    print("Congratualtions, it took you {} turns to guess the number {}".format(current_turn - 1, secret))
+else:
+    print("Sorry, you ran out of tries. Better luck next time!")
