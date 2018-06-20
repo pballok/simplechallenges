3acd6b881ec66f3156f17d4dbbc1741ae7dc1619
Peter Ballok <peter.ballok@logmein.com>
2018-06-20 09:34:00 +0200

    Implement suggested solution for problem math1

--- challenges/math1/math1_tests.cpp
+++ challenges/math1/math1_tests.cpp
@@ -17,10 +17,32 @@ SCENARIO("Calculate result with limit 0", "[Math1]")
 SCENARIO("Calculate result with limit divisible by 5 and 3", "[Math1]")
 {
     GIVEN("Adding all numbers divisible by 3 or 5, up to 30") {
-        auto result = challenges::sum_of_integers_divisible_by_3_or_5(0);
+        auto result = challenges::sum_of_integers_divisible_by_3_or_5(30);
 
         THEN("The result must be 225") {
             REQUIRE(result == 225);
+        }
+    }
+}
+
+SCENARIO("Calculate result with limit divisible by 3", "[Math1]")
+{
+    GIVEN("Adding all numbers divisible by 3 or 5, up to 3") {
+        auto result = challenges::sum_of_integers_divisible_by_3_or_5(3);
+
+        THEN("The result must be 3") {
+            REQUIRE(result == 3);
+        }
+    }
+}
+
+SCENARIO("Calculate result with limit divisible by 5", "[Math1]")
+{
+    GIVEN("Adding all numbers divisible by 3 or 5, up to 10") {
+        auto result = challenges::sum_of_integers_divisible_by_3_or_5(10);
+
+        THEN("The result must be 33") {
+            REQUIRE(result == 33);
         }
     }
 }
--- challenges/math1/solution1.cpp
+++ challenges/math1/solution1.cpp
@@ -2,5 +2,18 @@
 
 unsigned long challenges::sum_of_integers_divisible_by_3_or_5(unsigned int limit)
 {
-    return limit;
+    unsigned long sum = 0;
+
+    for(unsigned int i = 3; i <= limit; ++i) {
+        if(i % 3 == 0) {
+            sum += i;
+        }
+        else {
+            if(i % 5 == 0) {
+                sum += i;
+            }
+        }
+    }
+
+    return sum;
 }
