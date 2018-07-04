5590efc8155376062c99b7f3099c9938a7bb758b
Peter Ballok <pballok@pinkpanther-localdomain.3amlabs.net>
2018-07-04 15:00:17 +0200

    Implement vocab_test in Python

---
+++ games/vocab_test/vocab_test.py
@@ -0,0 +1,36 @@
+from random import shuffle
+
+def LoadQuestions(file_name, question_list, answer_list):
+    with open(file_name) as f:
+        for line in f.readlines():
+            parts = line.split('*')
+            if len(parts) == 2:
+                question_list.append(parts[0].strip())
+                answer_list.append(parts[1].strip())
+
+def AskAQuestion(questions, answers, questions_left):
+    number_of_remaining_questions = len(questions_left)
+
+    shuffle(questions_left)
+    next_question = questions_left[number_of_remaining_questions - 1]
+
+    answer = input('{} {} : '.format(number_of_remaining_questions, questions[next_question]))
+    if(answer == answers[next_question]):
+        del(questions_left[number_of_remaining_questions - 1])
+    else:
+        print('Wrong. The correct answer was: {}'.format(answers[next_question]))
+
+
+def main():
+    questions = []
+    answers = []
+    max_questions = 3
+
+    LoadQuestions('questions.txt', questions, answers)
+
+    questions_left = list(range(len(questions))) * max_questions
+
+    while questions_left:
+        AskAQuestion(questions, answers, questions_left)
+
+main()
