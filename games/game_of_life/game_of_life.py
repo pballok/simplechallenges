f0a1497464bb1d80f80753015c5abdd577513ad1
Peter Ballok <peter.ballok@logmein.com>
2018-09-11 20:53:25 +0200

    Add functionality to buttons

--- games/game_of_life/game_of_life.py
+++ games/game_of_life/game_of_life.py
@@ -2,6 +2,9 @@ import pygame
 import grid_oo
 import golbutton
 
+GRID_SIZE_X = 100
+GRID_SIZE_Y = 100
+
 SCREEN_SIZE_X = 860
 SCREEN_SIZE_Y = 740
 SCREEN_MARGIN = 20
@@ -9,74 +12,86 @@ CONTROL_AREA  = 100
 BUTTON_WIDTH = CONTROL_AREA
 BUTTON_HEIGHT = 20
 
-def drawCells(screen, grid, cell_size):
-    for position in range(grid.GRID_SIZE_N * grid.GRID_SIZE_M):
-        x = (position % grid.GRID_SIZE_N) * cell_size + SCREEN_MARGIN
-        y = (position // grid.GRID_SIZE_N) * cell_size + SCREEN_MARGIN
-        color = (255, 255, 255) if grid.isCellAlive(position) else (0, 0, 0)
-        pygame.draw.rect(screen, color, (x, y, cell_size, cell_size), 0)
+class Game:
 
-def step(grid):
-    pass
+    def __init__(self, screen, cell_size):
 
-def start():
-    pass
+        self.screen = screen
+        self.cell_size = cell_size
 
-def stop():
-    pass
+        self.exiting = False
+        self.playing = False
+        self.grid = grid_oo.Grid(GRID_SIZE_X, GRID_SIZE_Y)
 
-def exit():
-    pass
-    
+        self.grid.setCell(  2, 1)
+        self.grid.setCell(102, 1)
+        self.grid.setCell(202, 1)
+        self.grid.setCell(201, 1)
+        self.grid.setCell(100, 1)
+        self.drawCells()
+
+    def drawCells(self):
+        for position in range(self.grid.GRID_SIZE_N * self.grid.GRID_SIZE_M):
+            x = (position % self.grid.GRID_SIZE_N) * self.cell_size + SCREEN_MARGIN
+            y = (position // self.grid.GRID_SIZE_N) * self.cell_size + SCREEN_MARGIN
+            color = (255, 255, 255) if self.grid.isCellAlive(position) else (0, 0, 0)
+            pygame.draw.rect(self.screen, color, (x, y, self.cell_size, self.cell_size), 0)
+
+    def step(self):
+        self.grid.step()
+        self.drawCells()
+
+    def start(self):
+        self.playing = True
+
+    def stop(self):
+        self.playing = False
+
+    def exit(self):
+        self.exiting = True
+
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y))
-    done = False
 
-    grid = grid_oo.Grid(100, 100)
-
-    grid.setCell(  2, 1)
-    grid.setCell(102, 1)
-    grid.setCell(202, 1)
-    grid.setCell(201, 1)
-    grid.setCell(100, 1)
-
-    grid_size_x = int((SCREEN_SIZE_X - CONTROL_AREA - 3 * SCREEN_MARGIN) / grid.GRID_SIZE_N)
-    grid_size_y = int((SCREEN_SIZE_Y - 2 * SCREEN_MARGIN) / grid.GRID_SIZE_M)
+    grid_size_x = int((SCREEN_SIZE_X - CONTROL_AREA - 3 * SCREEN_MARGIN) / GRID_SIZE_X)
+    grid_size_y = int((SCREEN_SIZE_Y - 2 * SCREEN_MARGIN) / GRID_SIZE_Y)
     cell_size = min(grid_size_x, grid_size_y)
-    grid_width = cell_size * grid.GRID_SIZE_N
-    grid_height = cell_size * grid.GRID_SIZE_M
+    grid_width = cell_size * GRID_SIZE_X
+    grid_height = cell_size * GRID_SIZE_Y
 
     pygame.draw.rect(screen, (255, 255, 255), (SCREEN_MARGIN, SCREEN_MARGIN, grid_width, grid_height), 1)
 
+    game = Game(screen, cell_size)
+
     button_top = SCREEN_MARGIN
-    step_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Step", step)
+    step_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Step", game.step)
     button_top += BUTTON_HEIGHT + SCREEN_MARGIN
-    start_button = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Start", start)
+    start_button = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Start", game.start)
     button_top += BUTTON_HEIGHT + 5
-    stop_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Stop", stop)
+    stop_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Stop", game.stop)
     button_top += BUTTON_HEIGHT + SCREEN_MARGIN
-    exit_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Exit", exit)
-    
-    while not done:
+    exit_button  = golbutton.GOLButton((2 * SCREEN_MARGIN + grid_width, button_top, BUTTON_WIDTH, BUTTON_HEIGHT), "Exit", game.exit)
+
+    while not game.exiting:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                game.exit()
             else:
                 step_button.check_event(event)
                 start_button.check_event(event)
                 stop_button.check_event(event)
                 exit_button.check_event(event)
 
-        grid.step()
-        drawCells(screen, grid, cell_size)
+        if game.playing:
+            game.step()
 
         step_button.draw(screen)
         start_button.draw(screen)
         stop_button.draw(screen)
         exit_button.draw(screen)
-            
+
         pygame.display.flip()
 
 main()
